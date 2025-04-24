800a3c164f07806ec628b20511c6781116ea3a91
alberr22 <a.acebes.2021@alumnos.urjc.es>
2025-04-24 16:47:58 +0200

    header hecho

--- frontend/src/app/shared/header/header.component.ts
+++ frontend/src/app/shared/header/header.component.ts
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  items = [
+    { name: 'Home', icon: 'home', link: '/' },
+    { name: 'Clubbing', icon: 'information-circle', link: '/clubbing' },
+    { name: 'Conciertos', icon: 'musical-notes', link: '/concerts' },
+    { name: 'Eventos', icon: 'mail', link: '/events' },
+    { name: 'Contactanos', icon: 'party', link: '/contact' },
+    { name: 'Login', icon: 'people', link: '/modalLogin' }
+  ];
 }
