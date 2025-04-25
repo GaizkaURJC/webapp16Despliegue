3cd3e5b73706d7bdfacdff45f296e067d320d17c
Jon <jonmazh@gmail.com>
2025-04-25 19:50:48 +0200

    Concerts sin formato. Cargando ejemplos desde ts.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,21 +1,21 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { HomeComponent } from './pages/home/home.component'; // Ajusta esta ruta según tu estructura real
-import { ErrorComponent } from './shared/error/error.component'; //
-//  Ajusta esta ruta según tu estructura real
+import { HomeComponent } from './pages/home/home.component'; 
+import { ErrorComponent } from './shared/error/error.component'; 
+import { ConcertComponent } from './pages/concert/concert.component';
+
 export const routes: Routes = [
 
       { path: '', component: HomeComponent },    // Respuesta a /new
       { path: 'home', component: HomeComponent }, // Respuesta a /new/home
+      { path: 'concerts', component: ConcertComponent },
       { path: 'error', component: ErrorComponent }, // Respuesta a /new/error
   { path: '**', redirectTo: 'error' } // Redirige rutas no encontradas
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
-  declarations:[
-                
-                ], // Asegúrate de declarar los componentes aquí si no son standalone
+  declarations:[], // Asegúrate de declarar los componentes aquí si no son standalone
   providers: [],
   bootstrap:[] // Puedes agregar servicios aquí si es necesario
 })
