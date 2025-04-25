8be18655d26aed0fa527b5dc6ce4c440f6d469b0
RIKOK <a.rico7104@gmail.com>
2025-04-25 23:27:21 +0200

    arreglado el fallo de bootstrap para que funcione teneis que borrar la carpeta de node y volverla a bajar

--- frontend/src/app/shared/footer/footer.component.ts
+++ frontend/src/app/shared/footer/footer.component.ts
@@ -15,10 +15,10 @@ export class FooterComponent {
     { name: 'LinkedIn', icon: 'logo-linkedin' },
     { name: 'YouTube', icon: 'logo-youtube' }
   ];
-  Titulo=[{name:"titulo",content:"Teatro Daw"},
-          {name:"texto",content:"Somos una de la mayores discotecas de Madrid, con una capacidad de 6000 personas y una de las mejores ofertas de fiestas en la capital. También, ofertamos fiestas privadas y eventos de todo tipo."},
-          {name:"sala", content:"Sala DEGRADEV"}]
-  
+  Titulo = [{ name: "titulo", content: "Teatro Daw" },
+  { name: "texto", content: "Somos una de la mayores discotecas de Madrid, con una capacidad de 6000 personas y una de las mejores ofertas de fiestas en la capital. También, ofertamos fiestas privadas y eventos de todo tipo." },
+  { name: "sala", content: "Sala DEGRADEV" }]
+
   // Método para obtener el contenido por nombre
   getTituloContent(name: string): string | undefined {
     const item = this.Titulo.find(t => t.name === name);
@@ -26,16 +26,16 @@ export class FooterComponent {
   }
 
   links = [
-    "Pagina principal","Sobre Nosotros","Artistas", "Contacta con Nosotros"
+    "Pagina principal", "Sobre Nosotros", "Artistas", "Contacta con Nosotros"
   ]
 
   aditionalInfo = [
-    
+
     "Calle del Arenal, Madrid",
     "degradev@teatrodaw.com",
     "+34 123 456 789",
     "Horario de atención: Lunes a Viernes de 9:00 a 18:00"
   ]
-  
+
 
 }
